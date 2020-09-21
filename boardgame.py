dfa9e9f453590baa5feb7e9bfe828c173bf020bd
DESKTOP-T4UPVKC\adiab <adiabdulahovic@icloud.com>
2020-09-21 12:49:08 +0200

    Instructions added new file

---
+++ boardgame.py
@@ -0,0 +1,41 @@
+''' Del 1 - Samling över spel
+Du väljer själv vilka datastrukturer du vill ha och vilka filer du vill lagra i, men varje spel ska
+MINSTA ha följande egenskaper:
+1. Titel
+2. Antal spelare
+3. Tidsåtgång
+4. Rekommenderad ålder
+Om du själv kommer på något som också vore bra att ha med, så ta med det.
+Samlingen ska sparas på fil, så att den finns kvar från gång till gång med programmet. Ni
+ska inte skapa en sql-databas, utan använd som sagt olika datastrukturer och lagring på fil '''
+
+# Create a datastructure file (list, dict, tuples etc..) that saves these attributes from input
+#1. Titel
+#2. Antal spelare
+#3. Tidsåtgång
+#4. Rekommenderad ålder
+
+''' Del 2 - Inmatning
+ Vid inmatning av ett nytt spel är det viktigt att validera alla indata, det vill säga, se till att det
+som ska vara siffror är siffror. En rekommendation är också att se till att namnet är unikt.
+I det här läget rekommenderas du se till att inmatningen görs från
+kommandotolken/terminalen, men kom ihåg att skriva modulärt, så att det går att ändra i
+framtiden!
+Se till att det inskrivna spelet sparas på fil. '''
+
+# Make sure 
+
+# Del 3 - Lista program
+''' Programmet ska kunna visa en fullständig lista på alla spel som är inlagda. Den ska läsas in
+från fil. '''
+
+# Del 4 - Redigering av inlagda spel
+''' Programmet ska ha funktioner för att radera och redigera inlagda spel. Gör även det här så
+att det fungerar från kommandotolken/terminalen i nuläget. Dock behöver det vara
+självförklarande för en användare från första gången programmet används'''
+
+# Del 5 - Filtrering
+''' Användaren ska kunna fylla i en eller flera önskade egenskaper hos ett spel (till exempel
+antal spelare och speltid) och då få en lista på enbart de spel som uppfyller dessa kriterier'''
+
+# Del 6 - Utökningar
