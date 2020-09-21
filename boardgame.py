4d6e79483202fcf4f8c3a491735a005e31c1b2eb
DESKTOP-T4UPVKC\adiab <adiabdulahovic@icloud.com>
2020-09-21 15:38:39 +0200

    Updated content in files

--- boardgame.py
+++ boardgame.py
@@ -14,6 +14,10 @@ ska inte skapa en sql-databas, utan använd som sagt olika datastrukturer och la
 #2. Antal spelare
 #3. Tidsåtgång
 #4. Rekommenderad ålder
+# Steg 1: Skapa klass med dessa attributer
+# Steg 2: Lagra objekt i valfri datatyp
+# Steg 3: Skriv lagrade objekt till txt-fil
+
 
 ''' Del 2 - Inmatning
  Vid inmatning av ett nytt spel är det viktigt att validera alla indata, det vill säga, se till att det
@@ -23,19 +27,38 @@ kommandotolken/terminalen, men kom ihåg att skriva modulärt, så att det går 
 framtiden!
 Se till att det inskrivna spelet sparas på fil. '''
 
-# Make sure 
+# Make sure every input is correct. Example: convert number inputs to int. Save user input in file.
 
-# Del 3 - Lista program
-''' Programmet ska kunna visa en fullständig lista på alla spel som är inlagda. Den ska läsas in
+
+'''Del 3 - Lista program
+ Programmet ska kunna visa en fullständig lista på alla spel som är inlagda. Den ska läsas in
 från fil. '''
 
-# Del 4 - Redigering av inlagda spel
-''' Programmet ska ha funktioner för att radera och redigera inlagda spel. Gör även det här så
+# Print list of all games that exist in input-file
+
+'''Del 4 - Redigering av inlagda spel
+Programmet ska ha funktioner för att radera och redigera inlagda spel. Gör även det här så
 att det fungerar från kommandotolken/terminalen i nuläget. Dock behöver det vara
 självförklarande för en användare från första gången programmet används'''
 
-# Del 5 - Filtrering
-''' Användaren ska kunna fylla i en eller flera önskade egenskaper hos ett spel (till exempel
+# Create functions that will allow the user to delete and edit existing games (With instructions for user)
+
+'''Del 5 - Filtrering
+Användaren ska kunna fylla i en eller flera önskade egenskaper hos ett spel (till exempel
 antal spelare och speltid) och då få en lista på enbart de spel som uppfyller dessa kriterier'''
 
-# Del 6 - Utökningar
+# Create a function that allows the user to search for a game based on it's attributes
+
+'''Del 6 - Utökningar
+Om du har kommit så här långt, och gjort det på ett bra sätt, så har du klarat av den
+grundläggande uppgiften.
+Men för att lära dig mer och öka dina chanser för ett bra betyg så bör du vidareutveckla ditt
+program på något vis. Det finns många sätt att göra detta, och du väljer själv vilka du tycker
+är rimligt att prioritera. Här är några förslag:
+● Gör det möjligt att hantera flera olika personers spelsamlingar
+● Låt användaren registrera när ett spel spelats
+● Låt användaren sätta betyg på spel, och rangordna den visade listan efter dessa
+● Visa spel som bara nästan uppfyller kriterierna, men visa dem sist i listan
+Det går förstås också bra att hitta på egna förbättringsmöjligheter.'''
+
+# Create function that allows the user to rate the game and print the list of all games that are rated best.
